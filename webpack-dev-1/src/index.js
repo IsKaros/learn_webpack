9683597fb044b707efeb583b97334b1aafd45790
陈文帆 <819481291@qq.com>
2019-12-25 10:14:48 +0800

    initial commit

---
+++ webpack-dev-1/src/index.js
@@ -0,0 +1,28 @@
+// import './index.css'
+import $ from 'jquery'
+console.log(window.$,window.jQuery);
+// console.log($)
+// let test = require('./test')
+// require('./index.css')
+import './index.css'
+// console.log('webpack-learning')
+
+// let fn = () => {
+//   console.log('es6-syntax')
+// }
+// fn()
+
+// // @isTestable(true)
+// class MyClass { }
+
+// function isTestable(value) {
+//    return function decorator(target) {
+//       target.isTestable = value;
+//       console.log(target);
+//    }
+// }
+// function * gen(params) {
+//   yield 1
+// }
+// console.log(gen().next());
+// console.log(MyClass.isTestable);
---
+++ webpack-dev-1/webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
+const OptimizeCss= require('optimize-css-assets-webpack-plugin')
+const TerserJS = require('terser-webpack-plugin')
+const webpack = require('webpack')
+console.log(require.resolve('jquery'));
+module.exports = {
+  optimization:{ // 优化项
+    minimizer:[
+      new TerserJS({
+        exclude: /\/node_modules/,
+        parallel: true,
+      }),
+      new OptimizeCss({
+        filename:'main.css'
+      })
+    ]
+
+    
+  },
+  devServer:{
+    port:'3000',
+    contentBase:'./src',
+    progress:true,
+    compress:true,
+
+  },
+  mode: 'production', //模式有两种，development和production（开发模式和生产模式）
+  entry:"./src/index.js", // 入口文件，
+  output:{
+    filename:'bundle.js', // 打包出来的文件名 空配置/不写的情况下为main.js
+    path:path.resolve(__dirname,'dist'), // 打包的文件路径，必须为绝对路径
+  },
+  module:{
+    rules:[
+      // { test: require.resolve("jquery"), loader: "expose-loader?$" },
+      {
+        test: require.resolve('jquery'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'jQuery'
+        },{
+          loader: 'expose-loader',
+          options: '$'
+        }]
+      },
+      {
+        test:/\.js$/,
+        use:[{
+          loader:'eslint-loader',
+          
+          options:{
+
+          }
+        }],
+        exclude: /node_modules/,
+      },
+      {
+        test:/\.js$/,
+        include:path.resolve(__dirname,'src'),
+        exclude:/node_modules/,
+        use:[{
+          loader:'babel-loader',
+          options:{
+            presets:['@babel/preset-env'], // 允许使用最新版的JavaScript语法，不含处于草案任意阶段的语法
+            plugins:[
+              ["@babel/plugin-proposal-decorators", { "legacy": true }],// 允许使用未被正式写入规范的装饰器语法
+              ["@babel/plugin-proposal-class-properties", { "loose" : true }],
+              "@babel/plugin-transform-runtime",
+            ] 
+          }
+          
+        }]  
+      },
+    {
+      test:/\.css$/,
+      use:[{loader:MiniCssExtract.loader},{loader:'css-loader'}]
+    }]
+  },
+  plugins:[
+    new htmlWebpackPlugin({
+      template: './src/index.html',
+      minify:{
+        collapseWhitespace:true,
+        removeAttributeQuotes:true
+      },
+      hash:true
+    }),
+    // 配置和output差不多
+    new MiniCssExtract({
+      filename:'[name].css', 
+    }),
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery'
+    // })
+  ]
+}
