b99a08aff8a89216174af37b8b7fcc428ad996a9
陈文帆 <819481291@qq.com>
2019-12-29 16:02:34 +0800

    finish webpack-dev-2-learning

--- webpack-dev-2/server.js
+++ webpack-dev-2/server.js
@@ -1,9 +1,13 @@
 var express = require('express')
+const webpack = require('webpack')
+const middle = require('webpack-dev-middleware');
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
 
 let app = new express()
+app.use(middle(compiler))
+
 app.get('/webpack',function (req,res) {
   res.json({todo:'learning-webpack'})
 })
-app.listen('3000',function name(params) {
-  
-})
+app.listen('3000')
--- webpack-dev-2/src/index.js
+++ webpack-dev-2/src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap'
 // console.log('continue learing webpack');
 // console.lo('a error');
 // class log {
@@ -6,9 +7,14 @@
 //     console.lo('error');
 //   }
 // }
+if(DEV === true){
+  console.log('development')
+}else {
+  console.log('production');
+}
 let xhr = new XMLHttpRequest()
 xhr.open('get','/api/webpack',true)
-xhr.send()
 xhr.onload = function name(e) {
   console.log(xhr.response);
 }
+xhr.send()
